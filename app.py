4d90a78f695d0aa10f4309b24369f395fca82eab
Jérémy Martin <ynphea@gmail.com>
2024-01-13 22:37:39 +0100

    add table and rates

--- app.py
+++ app.py
@@ -4,25 +4,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-def get_cryptocurrencies(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    crypto_table = soup.find('table', {'class': 'cmc-table'})  # Adjust class as needed
-    cryptos = []
-
-    for row in crypto_table.find_all('tr')[1:]:  # Skip header
-        columns = row.find_all('td')
-        name = columns[2].text.strip()  # Adjust index as needed
-        price = columns[3].text.strip()  # Adjust index as needed
-        rate = columns[4].text.strip()  # Adjust index as needed
-        cryptos.append((name, price, rate))
-
-    return cryptos
-
 @app.route('/')
 def show_cryptocurrencies():
-    url = 'https://coinmarketcap.com/'
-    cryptos = get_cryptocurrencies(url)
+    cryptos = [
+        ('assets/gala.png', 'GALA', '94.59', '74'),
+        ('assets/apt.png', 'APT', '94.28', '334'),
+        ('assets/atom.png', 'ATOM', '94.25', '392'),
+        ('assets/xrp.png', 'XRP', '94.25', '3124'),
+        ('assets/wemix.png', 'WEMIX', '93.91', '1462'),
+        ('assets/paxg.png', 'PAXG', '93.61', '42'),
+        ('assets/cake.png', 'CAKE', '93.49', '72'),
+        ('assets/sand.png', 'SAND', '93.39', '108'),
+        ('assets/matic.png', 'MATIC', '93.27', '844'),
+        ('assets/1inch.png', '1INCH', '93.07', '56'),
+        ('assets/ton.png', 'TON', '92.91', '731')
+    ]
     return render_template('cryptos.html', cryptos=cryptos)
 
 if __name__ == '__main__':
