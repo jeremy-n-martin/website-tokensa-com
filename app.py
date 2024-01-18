cf69c2b44fc20111a72b22c662297cf25ffe3d23
Jérémy Martin <jeremy.martin@smart-chain.fr>
2024-01-18 14:28:15 +0100

    added style

--- app.py
+++ app.py
@@ -60,11 +60,11 @@ def get_crypto_prices(symbols):
                     price_change_24h = "{:.2f}".format(quote['percent_change_24h']) if quote['percent_change_24h'] is not None else None
                     price_change_7d = "{:.2f}".format(quote['percent_change_7d']) if quote['percent_change_7d'] is not None else None
                     market_cap = int(quote['market_cap']/1000000) if quote['market_cap'] is not None else None
-                    prices[symbol] = (price if price is not None else None, 
+                    prices[symbol] = (market_cap if market_cap is not None else None,
+                                      price if price is not None else None, 
                                       price_change_1h if price_change_1h is not None else None,
                                       price_change_24h if price_change_24h is not None else None,
-                                      price_change_7d if price_change_7d is not None else None,
-                                      market_cap if market_cap is not None else None)
+                                      price_change_7d if price_change_7d is not None else None)
                 else:
                     logging.warning(f"Symbol {symbol} not found in API response")
                     prices[symbol] = (None, None, None, None, None)
@@ -81,75 +81,94 @@ def get_crypto_prices(symbols):
 def index(column='market_cap', order='desc'):
 
     cryptos = [
-        ('assets/btc.png', 'BTC'    , '96.87' ),
-        ('assets/eth.png', 'ETH'    , '96.23' ),
-        ('assets/bnb.png', 'BNB'    , '92.57' ),
-        ('assets/sol.png', 'SOL'    , '91.96' ),
-        ('assets/xrp.png', 'XRP'    , '94.25' ),
-        ('assets/ada.png', 'ADA'    , '89.19' ),
-        ('assets/avax.png', 'AVAX'  , '94.12' ),
-        ('assets/doge.png', 'DOGE'  , '86.88' ),
-        ('assets/trx.png', 'TRX'    , '88.20' ),
-        ('assets/dot.png', 'DOT'    , '83.80' ),
-        ('assets/link.png', 'LINK'  , '87.03' ),
-        ('assets/icp.png', 'ICP'    , '81.78' ),
-        ('assets/wemix.png', 'WEMIX', '93.91' ),
-        ('assets/matic.png', 'MATIC', '93.27' ),
-        ('assets/ton.png', 'TON'    , '92.91' ),
-        ('assets/shib.png', 'SHIB'  , '90.81' ),
-        ('assets/uni.png', 'UNI'    , '94.28' ),
-        ('assets/atom.png', 'ATOM'  , '94.25' ),
-        ('assets/ltc.png', 'LTC'    , '77.24' ),
-        ('assets/bch.png', 'BCH'    , '74.16' ),
-        ('assets/leo.png', 'LEO'    , '86.06' ),
-        ('assets/inj.png', 'INJ'    , '84.13' ),
-        ('assets/xlm.png', 'XLM'    , '85.97' ),
-        ('assets/near.png', 'NEAR'  , '90.96' ),
-        ('assets/op.png', 'OP'      , '88.60' ),
-        ('assets/okb.png', 'OKB'    , '92.69' ),
-        ('assets/apt.png', 'APT'    , '94.28' ),
-        ('assets/tia.png', 'TIA'    , '69.14' ),
-        ('assets/xmr.png', 'XMR'    , '73.35' ),
-        ('assets/ldo.png', 'LDO'    , '91.64' ),
-        ('assets/fil.png', 'FIL'    , '79.86' ),
-        ('assets/arb.png', 'ARB'    , '94.30' ),
-        ('assets/hbar.png', 'HBAR'  , '83.21' ),
-        ('assets/imx.png', 'IMX'    , '89.76' ),
-        ('assets/sei.png', 'SEI'    , '90.37' ),
-        ('assets/rune.png', 'RUNE'  , '89.75' ),
-        ('assets/sand.png', 'SAND'  , '93.39' ),
-        ('assets/theta.png', 'THETA', '91.59' ),
-        ('assets/chz.png', 'CHZ'    , '92.30' ),
-        ('assets/gala.png', 'GALA'  , '94.59' ),
-        ('assets/cake.png', 'CAKE'  , '93.49' ),
-        ('assets/frax.png', 'FRAX'  , '91.59' ),
-        ('assets/1inch.png', '1INCH', '93.07' ),
-        ('assets/fet.png', 'FET'    , '91.97' ),
-        ('assets/axl.png', 'AXL'    , '92.41' ),
-        ('assets/twt.png', 'TWT'    , '92.84' ),
-        ('assets/core.png', 'CORE'  , '90.39' ),
-        ('assets/enj.png', 'ENJ'    , '92.08' ),
-        ('assets/ftn.png', 'FTN'    , '90.45' ),
-        ('assets/paxg.png', 'PAXG'  , '93.61' ),
-        ('assets/iotx.png', 'IOTX'  , '92.30' ),
-        ('assets/trb.png', 'TRB'    , '91.30' ),
-        ('assets/floki.png', 'FLOKI', '91.93' ),
-        ('assets/sxp.png', 'SXP'    , '91.62' ),
-        ('assets/sfund.png', 'SFUND', '90.11' ),
+        ('assets/btc.png', 'BTC'    , '96.87', '0' ),
+        ('assets/eth.png', 'ETH'    , '96.23', '0' ),
+        ('assets/bnb.png', 'BNB'    , '92.57', '0' ),
+        ('assets/sol.png', 'SOL'    , '91.96', '0' ),
+        ('assets/xrp.png', 'XRP'    , '94.25', '0' ),
+        ('assets/ada.png', 'ADA'    , '89.19', '0' ),
+        ('assets/avax.png', 'AVAX'  , '94.12', '0' ),
+        ('assets/doge.png', 'DOGE'  , '86.88', '0' ),
+        ('assets/trx.png', 'TRX'    , '88.20', '0' ),
+        ('assets/dot.png', 'DOT'    , '83.80', '0' ),
+        ('assets/link.png', 'LINK'  , '87.03', '0' ),
+        ('assets/icp.png', 'ICP'    , '81.78', '0' ),
+        ('assets/wemix.png', 'WEMIX', '93.91', '0' ),
+        ('assets/matic.png', 'MATIC', '93.27', '0' ),
+        ('assets/ton.png', 'TON'    , '92.91', '0' ),
+        ('assets/shib.png', 'SHIB'  , '90.81', '0' ),
+        ('assets/uni.png', 'UNI'    , '94.28', '0' ),
+        ('assets/atom.png', 'ATOM'  , '94.25', '0' ),
+        ('assets/ltc.png', 'LTC'    , '77.24', '0' ),
+        ('assets/bch.png', 'BCH'    , '74.16', '0' ),
+        ('assets/leo.png', 'LEO'    , '86.06', '0' ),
+        ('assets/inj.png', 'INJ'    , '84.13', '0' ),
+        ('assets/xlm.png', 'XLM'    , '85.97', '0' ),
+        ('assets/near.png', 'NEAR'  , '90.96', '0' ),
+        ('assets/op.png', 'OP'      , '88.60', '0' ),
+        ('assets/okb.png', 'OKB'    , '92.69', '0' ),
+        ('assets/apt.png', 'APT'    , '94.28', '0' ),
+        ('assets/tia.png', 'TIA'    , '69.14', '0' ),
+        ('assets/xmr.png', 'XMR'    , '73.35', '0' ),
+        ('assets/ldo.png', 'LDO'    , '91.64', '0' ),
+        ('assets/fil.png', 'FIL'    , '79.86', '0' ),
+        ('assets/arb.png', 'ARB'    , '94.30', '0' ),
+        ('assets/vet.png', ''    , '0', '0' ),
+        ('assets/mkr.png', ''    , '0', '0' ),
+        ('assets/rndr.png', ''    , '0', '0' ),
+        ('assets/bsv.png', ''    , '0', '0' ),
+        ('assets/grt.png', ''    , '0', '0' ),
+        ('assets/algo.png', ''    , '0', '0' ),
+        ('assets/ordi.png', ''    , '0', '0' ),
+        ('assets/aave.png', ''    , '0', '0' ),
+        ('assets/egld.png', ''    , '0', '0' ),
+        ('assets/qnt.png', ''    , '0', '0' ),
+        ('assets/sui.png', ''    , '0', '0' ),
+        ('assets/mina.png', ''    , '0', '0' ),
+        ('assets/sats.png', ''    , '0', '0' ),
+        ('assets/flow.png', ''    , '0', '0' ),
+        ('assets/hnt.png', ''    , '0', '0' ),
+        ('assets/ftm.png', ''    , '0', '0' ),
+        ('assets/snx.png', ''    , '0', '0' ),
+        ('assets/axs.png', ''    , '0', '0' ),
+        ('assets/hbar.png', 'HBAR'  , '83.21', '0' ),
+        ('assets/imx.png', 'IMX'    , '89.76', '0' ),
+        ('assets/sei.png', 'SEI'    , '90.37', '0' ),
+        ('assets/rune.png', 'RUNE'  , '89.75', '0' ),
+        ('assets/sand.png', 'SAND'  , '93.39', '0' ),
+        ('assets/theta.png', 'THETA', '91.59', '0' ),
+        ('assets/chz.png', 'CHZ'    , '92.30', '0' ),
+        ('assets/gala.png', 'GALA'  , '94.59', '0' ),
+        ('assets/cake.png', 'CAKE'  , '93.49', '0' ),
+        ('assets/frax.png', 'FRAX'  , '91.59', '0' ),
+        ('assets/1inch.png', '1INCH', '93.07', '0' ),
+        ('assets/fet.png', 'FET'    , '91.97', '0' ),
+        ('assets/axl.png', 'AXL'    , '92.41', '0' ),
+        ('assets/twt.png', 'TWT'    , '92.84', '0' ),
+        ('assets/core.png', 'CORE'  , '90.39', '0' ),
+        ('assets/enj.png', 'ENJ'    , '92.08', '0' ),
+        ('assets/ftn.png', 'FTN'    , '90.45', '0' ),
+        ('assets/paxg.png', 'PAXG'  , '93.61', '0' ),
+        ('assets/iotx.png', 'IOTX'  , '92.30', '0' ),
+        ('assets/trb.png', 'TRB'    , '91.30', '0' ),
+        ('assets/floki.png', 'FLOKI', '91.93', '0' ),
+        ('assets/sxp.png', 'SXP'    , '91.62', '0' ),
+        ('assets/sfund.png', 'SFUND', '90.11', '0' ),
     ]
     
     symbols = [crypto[1] for crypto in cryptos]
     prices = get_crypto_prices(symbols)
-
     updated_cryptos = []
+
     for crypto in cryptos:
         symbol = crypto[1]
-        price, change_1h, change_24h, change_7d, market_cap = prices.get(symbol, (None, None, None, None, None))
-        rate_class = get_class_for_rate(float(crypto[2]))
+        market_cap, price, change_1h, change_24h, change_7d = prices.get(symbol, (None, None, None, None, None))
+        rate_ck_class = get_class_for_rate(float(crypto[2]))
+        rate_cb_class = get_class_for_rate(float(crypto[3]))
         change_1h_class = get_class_for_change(change_1h, scale=1)
         change_24h_class = get_class_for_change(change_24h, scale=5)
         change_7d_class = get_class_for_change(change_7d, scale=10)
-        updated_cryptos.append(crypto + (price, change_1h, change_24h, change_7d, market_cap, rate_class, change_24h_class, change_1h_class, change_7d_class))
+        updated_cryptos.append(crypto + (price, change_1h, change_24h, change_7d, market_cap, rate_ck_class, rate_cb_class, change_24h_class, change_1h_class, change_7d_class))
 
 
     if column and order:
@@ -158,8 +177,8 @@ def index(column='market_cap', order='desc'):
         def get_sort_key(crypto, col):
             """Helper function to get the correct sort key based on column."""
             column_keys = {
-                'rate': 2, 'price': 3, 'change_1h': 4, 'change_24h': 5,
-                'change_7d': 6, 'market_cap': 7
+                'rate_ck': 2, 'rate_cb': 3, 'price': 4, 'change_1h': 5, 'change_24h': 6,
+                'change_7d': 7, 'market_cap': 8
             }
 
 
